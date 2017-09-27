7097dac78aaca258e2eca8cb0765c428595f8529
CarlosShui <cshuizha7@alumnes.ub.edu>
2017-09-27 10:54:38 +0200

    adsfafd
    prueba

---
+++ JoseDaniCarlos_TotSeries.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package josedanicarlos_totseries;
+
+/**
+ *
+ * @author carlosshui
+ */
+public class JoseDaniCarlos_TotSeries {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
